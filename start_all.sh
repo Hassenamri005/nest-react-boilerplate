07659d8e2381f4b59ab78992edcc1537d7f579e3
Hassen Amri <hassenamri005@gmail.com>
2026-04-19 10:21:23 +0100

    feat: add supabase

--- start_all.sh
+++ start_all.sh
@@ -2,31 +2,25 @@
 echo "................ [INFO] Preparing BACKEND infrastructure ................"
 
 cd backend/
-
 cat .env.example >> .env
-
 echo -e  >> .env
-
 npm i
-
 docker-compose up --build -d
-
 npm run swagger:ts
-
 echo "[INFO] Preparing Backend infrastructure done."
 
 
 echo "................ [INFO] Preparing FRONTEND infrastructure ................"
 cd ../frontend
-
 rm .env
 cat .env.example >> .env
 echo -e  >> .env
-echo -e VITE_BACKEND_API_URL=$(gp url 6001) >> .env
-
+echo -e VITE_BACKEND_API_URL=http://localhost:6001 >> .env
 cp -f ../backend/src/api/myApi.ts ./src/api/myApi.ts
-
 npm i
 docker-compose --profile dev up --build -d
 echo "[INFO] Preparing FRONTEND infrastructure done."
 
+echo "................ [INFO] Preparing Supabase infrastructure ................"
+cd ../supabase
+echo "[INFO] Preparing Supabase infrastructure done."
