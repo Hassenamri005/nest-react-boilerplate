d03814351800cf656736217d57b7b8db00c16d68
Hassen Amri <hassenamri005@gmail.com>
2026-04-19 10:29:47 +0100

    refactor: update environment configuration and improve docker commands

--- start_all.sh
+++ start_all.sh
@@ -2,10 +2,11 @@
 echo "................ [INFO] Preparing BACKEND infrastructure ................"
 
 cd backend/
+rm .env
 cat .env.example >> .env
 echo -e  >> .env
 npm i
-docker-compose up --build -d
+docker compose up --build -d
 npm run swagger:ts
 echo "[INFO] Preparing Backend infrastructure done."
 
@@ -18,9 +19,13 @@ echo -e  >> .env
 echo -e VITE_BACKEND_API_URL=http://localhost:6001 >> .env
 cp -f ../backend/src/api/myApi.ts ./src/api/myApi.ts
 npm i
-docker-compose --profile dev up --build -d
+docker compose --profile dev up --build -d
 echo "[INFO] Preparing FRONTEND infrastructure done."
 
 echo "................ [INFO] Preparing Supabase infrastructure ................"
 cd ../supabase
+rm .env
+cat .env.example >> .env
+echo -e  >> .env
+docker compose up --build -d
 echo "[INFO] Preparing Supabase infrastructure done."
